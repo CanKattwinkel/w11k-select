e2d7bf90873729c4432a20377c873bd18248c473
Can Kattw <kattwinkel@w11k.de>
2017-05-10 07:47:47 +0200

    adds support for tab/space/enter select

--- src/w11k-select.ts
+++ src/w11k-select.ts
@@ -8,6 +8,7 @@ import {w11kSelect} from './w11k-select.directive';
 import {w11kSelectOptionDirektive} from './w11k-select-option/w11k-select-option.directive';
 import {w11kSelectCheckboxDirective} from './w11k-select-checkbox/w11k-checkbox.directive';
 import {w11kSelectInfiniteScroll} from './w11k-select-infinite-scroll.directive';
+import {keyListener} from './lib/key-listener';
 
 export const module: IModule = angular.module('w11k.select', [
   'w11k.dropdownToggle',
@@ -20,4 +21,5 @@ module
     .service('w11kSelectHelper', W11KSelectHelper)
     .directive('w11kSelect', w11kSelect)
     .directive('w11kSelectOption', w11kSelectOptionDirektive)
-    .directive('w11kSelectCheckbox', w11kSelectCheckboxDirective);
+    .directive('w11kSelectCheckbox', w11kSelectCheckboxDirective)
+    .directive('keyListener', keyListener);
